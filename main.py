eb5f8d9de23b5135dec1b7501505dc20724cc371
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-04-08 20:54:23 +0530

    Image Screen

--- main.py
+++ main.py
@@ -2,21 +2,30 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from FileSharer import FileSharer
+import time
 
 Builder.load_file('Frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
-        pass
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%M%D-%H%M%"S')
+        filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filepath)
+        self.manager.current = 'image_screen'
+        self.manager.current_screen.ids.img.source = filepath
 
 class ImageScreen(Screen):
-    pass
+    def create_link(self)
 
 class RootWidget(ScreenManager):
     pass
