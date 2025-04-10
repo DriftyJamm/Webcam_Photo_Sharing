15eb4134bef6226e33d18dc9439759de920e8432
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-04-10 20:28:31 +0530

    Copy,Open Link button added

--- main.py
+++ main.py
@@ -2,30 +2,51 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from FileSharer import FileSharer
+from kivy.core.clipboard import Clipboard
 import time
+import webbrowser
 
 Builder.load_file('Frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
+        self.ids.camera.opacity = 1
         self.ids.camera.play = True
         self.ids.camera_button.text = "Stop Camera"
         self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
+        self.ids.camera.opacity = 0
         self.ids.camera.play = False
         self.ids.camera_button.text = "Start Camera"
         self.ids.camera.texture = None
 
     def capture(self):
         current_time = time.strftime('%Y%M%D-%H%M%"S')
-        filepath = f"files/{current_time}.png"
-        self.ids.camera.export_to_png(filepath)
+        self.filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(self.filepath)
         self.manager.current = 'image_screen'
-        self.manager.current_screen.ids.img.source = filepath
+        self.manager.current_screen.ids.img.source = self.filepath
 
 class ImageScreen(Screen):
-    def create_link(self)
+    link_message = "Create a link First"
+    def create_link(self):
+        file_path = App.get_running_app().root.ids.camera_screen.filepath
+        filesharer = FileSharer(filepath = file_path)
+        self.url = filesharer.share()
+        self.ids.link.text = self.url
+    
+    def copy_link(self):
+        try:
+            Clipboard.copy(self.url)
+        except:
+            self.ids.link.text = self.link_message
+
+    def open_link(self):
+        try:
+            webbrowser.open(self.url)
+        except:
+            self.ids.link.text = self.link_message
 
 class RootWidget(ScreenManager):
     pass
